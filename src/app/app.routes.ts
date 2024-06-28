f9545b65ebf8fa90a147d501ea7f267bd2e557e2
Fabianaaguero <fabiana.aguero22@gmail.com>
2024-06-28 14:50:37 -0300

    Ult.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,12 +6,12 @@ import { InformacionComponent } from './componentes/informacion/informacion.comp
 import { QrComponent } from './componentes/qr/qr.component';
 
 export const routes: Routes = [
-  { path: '', component: InicioComponent },        // Ruta por defecto
+  { path: '', component: InicioComponent },
   { path: 'contacto', component: ContactoComponent },
   { path: 'informacion', component: InformacionComponent },
   { path: 'inicio', component: InicioComponent },
-  { path: 'qr', component: QrComponent },          // Ruta para el componente QrComponent
-  { path: '**', component: InicioComponent },      // Ruta para cualquier otra ruta no definida
+  { path: 'qr', component: QrComponent }, 
+  { path: '**', component: InicioComponent },
 ];
 
 @NgModule({
